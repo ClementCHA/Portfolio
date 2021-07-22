005af47de43ac0dd1210f9c51e27784cfffe6c17
ClementCHA <Clement.charlesC@gmail.com>
2021-07-23 01:33:02 +0200

    try to fixe cores

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 5000;
 const nodemailer = require('nodemailer');
 
 const app = express();
-app.use(cors());
+app.use(cors("*"));
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-}) 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept" Authorization');
+  res.header('Access-Control-Expose-Headers', 'Access-Token, Uid');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
 app.use(express.json());
 app.use('/', router);
 app.listen(PORT, () => console.log('Server Running'));
