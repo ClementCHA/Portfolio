4b4da52bfdaeb2131993d0510b976de671a51739
ClementCHA <Clement.charlesC@gmail.com>
2021-07-22 21:15:53 +0200

    Heroku nodemailer cors fix

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(cors());
-
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+}) 
 app.use(express.json());
 app.use('/', router);
 app.listen(PORT, () => console.log('Server Running'));
